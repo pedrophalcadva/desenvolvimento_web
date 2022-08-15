529dc97a8fe872ebae92dc4f577bde412fee5b34
Pedro PH Alca DVA <pedro_has12@hotmail.com>
2022-08-15 11:21:57 -0300

    desemvolvimento web

---
+++ Main04.java
@@ -0,0 +1,14 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    
+    //Operadores de Comparação
+    
+    int a = 200;
+    int b = 30;
+    
+    boolean resultado = a > b;
+    System.out.println("Resultado: " + resultado);
+		
+	}
+}
---
+++ Main05.java
@@ -0,0 +1,14 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    
+    //Operadores de Comparação
+    
+    final int a = 200;
+    final int b = 30;
+    
+    boolean resultado = a == b;
+    System.out.println("Resultado: " + resultado);
+		
+	}
+}
